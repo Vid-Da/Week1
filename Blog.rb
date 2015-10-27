fe7d47533f2ac32ca622a8d03f7d45165fa7d9a0
Vid-Da <d.encinas.t@gmail.com>
2015-10-27 19:07:19 +0100

    Advanced Blog

--- Blog.rb
+++ Blog.rb
@@ -15,29 +15,34 @@ class Blog
 
 	def create_front_page
 		order_by_date
-		publish_front_page	
+		publish_front_page
 	end
 
 	def publish_front_page
-		front_page = @posts.each do |post|
-			puts post.title + post.text + " " + post.date
+		@posts.each do |post|
+			if post.sponsored == "y"
+				puts "********* #{post.title} ***********  \n" + post.text + " " + post.date
+			else
+				puts post.title + "\n" + post.text + " " + post.date
+			end
 		end
 	end
 end
 
 class Post
-	attr_reader :title, :date, :text
-	def initialize(title, date, text)
+	attr_reader :title, :date, :text, :sponsored
+	def initialize(title, date, text, sponsored)
 		@title = title
 		@date = date
 		@text = text
+		@sponsored = sponsored
 	end
 end
 
 blog = Blog.new
-blog.add_post Post.new("Tercera ", "08/25/2015", " Aqui estamos")
-blog.add_post Post.new("Segunda ", "08/30/2015", " Este deberia ser el segundo")
-blog.add_post Post.new("Primera ", "08/31/2015", " Este deberia ser el primero")
+blog.add_post Post.new("Tercera ", "08/25/2015", "Aqui estamos", "0")
+blog.add_post Post.new("Segunda ", "08/30/2015", "Este deberia ser el segundo", "0")
+blog.add_post Post.new("Primera ", "08/31/2015", "Este deberia ser el primero", "0")
+blog.add_post Post.new("Cuarto", "05/05/2015", "Estoy sponsorizado!", "y")
 
-blog.publish_front_page
 blog.create_front_page
